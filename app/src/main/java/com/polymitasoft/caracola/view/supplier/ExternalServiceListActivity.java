af7cd3f75fb1b385d136fd722beea5408c3f715d
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2017-02-25 02:02:55 -0500

    Usar RecyclerListActivity para la lista de servicios externos.

--- app/src/main/java/com/polymitasoft/caracola/view/supplier/ExternalServiceListActivity.java
+++ app/src/main/java/com/polymitasoft/caracola/view/supplier/ExternalServiceListActivity.java
@@ -1,19 +1,16 @@
 package com.polymitasoft.caracola.view.supplier;
 
+import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.View;
-import android.view.ViewGroup;
 
 import com.polymitasoft.caracola.R;
 import com.polymitasoft.caracola.components.RecyclerListActivity;
+import com.polymitasoft.caracola.components.SimpleListAdapter;
 import com.polymitasoft.caracola.components.SimpleViewHolder;
-import com.polymitasoft.caracola.datamodel.IExternalService;
-
-import java.util.Random;
+import com.polymitasoft.caracola.datamodel.ExternalService;
 
 import io.requery.android.QueryRecyclerAdapter;
 import io.requery.query.Result;
@@ -22,71 +19,61 @@ import io.requery.query.Result;
  * @author rainermf
  * @since 16/2/2017
  */
-public class ExternalServiceListActivity extends RecyclerListActivity<IExternalService> {
+public class ExternalServiceListActivity extends RecyclerListActivity<ExternalService> {
 
     @Override
-    protected QueryRecyclerAdapter<IExternalService, SimpleViewHolder> createAdapter() {
-        return new ServiceAdapter();
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setBarTitle(R.string.title_external_services);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.list_add_menu, menu);
-        return true;
+    protected QueryRecyclerAdapter<ExternalService, SimpleViewHolder> createAdapter() {
+        return new ServiceAdapter(this);
     }
 
     @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.action_plus:
-                Intent intent = new Intent(this, ExternalServiceEditActivity.class);
-                startActivity(intent);
-                return true;
-        }
-        return false;
+    protected void onActionPlusMenu() {
+        Intent intent = new Intent(this, ExternalServiceEditActivity.class);
+        startActivity(intent);
     }
 
     /**
      * Created by rainermf on 15/2/2017.
      */
-    class ServiceAdapter extends QueryRecyclerAdapter<IExternalService, SimpleViewHolder> implements View.OnClickListener {
+    static class ServiceAdapter extends SimpleListAdapter<ExternalService> {
 
-        private final Random random = new Random();
-        private final int[] colors = {Color.RED, Color.BLUE, Color.GREEN, Color.MAGENTA};
-
-        ServiceAdapter() {
-//            super(ExternalServiceEntity.$TYPE);
+        ServiceAdapter(Context context) {
+            super(context, ExternalService.$TYPE);
         }
 
         @Override
-        public Result<IExternalService> performQuery() {
-            return data.select(IExternalService.class).get();
+        public Result<ExternalService> performQuery() {
+            return dataStore.select(ExternalService.class).get();
         }
 
         @Override
-        public void onBindViewHolder(IExternalService item, SimpleViewHolder holder, int position) {
+        public void onBindViewHolder(final ExternalService item, SimpleViewHolder holder, int position) {
+            super.onBindViewHolder(item, holder, position);
             holder.primaryText.setText(item.getName());
-            holder.colorStrip.setBackgroundColor(colors[random.nextInt(colors.length)]);
-            holder.itemView.setTag(item);
+            holder.editMenu.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editItem(item);
+                }
+            });
+            holder.itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editItem(item);
+                }
+            });
         }
 
-        @Override
-        public SimpleViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            LayoutInflater inflater = LayoutInflater.from(parent.getContext());
-            View view = inflater.inflate(R.layout.simple_list_item, null);
-            SimpleViewHolder holder = new SimpleViewHolder(view);
-            view.setOnClickListener(this);
-            return holder;
-        }
-
-        @Override
-        public void onClick(View v) {
-            IExternalService service = (IExternalService) v.getTag();
-            if (service != null) {
-                Intent intent = new Intent(ExternalServiceListActivity.this, ExternalServiceEditActivity.class);
-                intent.putExtra(ExternalServiceEditActivity.EXTRA_SERVICE_ID, service.getId());
-                startActivity(intent);
-            }
+        private void editItem(ExternalService service) {
+            Intent intent = new Intent(context, ExternalServiceEditActivity.class);
+            intent.putExtra(ExternalServiceEditActivity.EXTRA_SERVICE_ID, service.getId());
+            context.startActivity(intent);
         }
     }
 }
