5dd7466496c8fa384a33eb06d1a8fce17259be8e
asio <asio@polymitasoft.com>
2017-02-25 15:21:39 -0700

    Commit para unir con yanio

---
+++ app/src/main/java/com/polymitasoft/caracola/communication/GestionPrerreserva.java
@@ -0,0 +1,10 @@
+package com.polymitasoft.caracola.communication;
+
+/**
+ * Created by asio on 2/25/2017.
+ */
+
+public class GestionPrerreserva {
+
+    private static final String MENSAJE_CONFIRMACION = "Mensaje Prerreserva Recibido";
+}
