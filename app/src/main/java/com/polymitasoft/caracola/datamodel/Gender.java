09123218d1bc69cd361c04dc8819b7e3aa78116c
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2017-03-03 16:27:56 -0500

    Añadir selector de fecha y spinner para género en EditarCliente

--- app/src/main/java/com/polymitasoft/caracola/datamodel/Gender.java
+++ app/src/main/java/com/polymitasoft/caracola/datamodel/Gender.java
@@ -1,8 +1,18 @@
 package com.polymitasoft.caracola.datamodel;
 
+import com.polymitasoft.caracola.CaracolaApplication;
+
 /**
  * Created by rainermf on 11/2/2017.
  */
 public enum Gender {
-    FEMININE, MASCULINE
+    FEMININE, MASCULINE;
+
+    @Override
+    public String toString() {
+        // TODO Cambiar por una solución localizada
+        if(this == FEMININE)
+            return "Femenino";
+        return "Masculino";
+    }
 }
--- app/src/main/java/com/polymitasoft/caracola/view/client/ClientBinding.java
+++ app/src/main/java/com/polymitasoft/caracola/view/client/ClientBinding.java
@@ -1,14 +1,19 @@
 package com.polymitasoft.caracola.view.client;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 
+import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
 import com.polymitasoft.caracola.R;
 import com.polymitasoft.caracola.datamodel.Client;
-import com.polymitasoft.caracola.datamodel.IClient;
 import com.polymitasoft.caracola.datamodel.Country;
 import com.polymitasoft.caracola.datamodel.Gender;
 import com.polymitasoft.caracola.util.FormatUtils;
+
+import org.threeten.bp.LocalDate;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,25 +24,51 @@ import butterknife.ButterKnife;
  */
 class ClientBinding {
 
+    private final AppCompatActivity activity;
     @BindView(R.id.passportText) EditText passport;
     @BindView(R.id.firstNameText) EditText firstName;
     @BindView(R.id.lastNameText) EditText lastName;
     @BindView(R.id.birthdayText) EditText birthday;
-    @BindView(R.id.genderText) EditText gender;
+    @BindView(R.id.genderSpinner) Spinner gender;
     @BindView(R.id.countryText) EditText country;
+    private CalendarDatePickerDialogFragment birthdayPicker;
     private Client client;
 
-    ClientBinding(Activity activity, Client client) {
+    ClientBinding(AppCompatActivity activity, Client client) {
         ButterKnife.bind(this, activity);
+        this.activity = activity;
+        initPickers();
         setClient(client);
+    }
+
+    private void initPickers() {
+        birthdayPicker = new CalendarDatePickerDialogFragment()
+                .setOnDateSetListener(new CalendarDatePickerDialogFragment.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
+                        LocalDate date = LocalDate.of(year, monthOfYear + 1, dayOfMonth);
+                        birthday.setText(FormatUtils.formatDate(date));
+                        client.setBirthday(date);
+                    }
+                });
+        birthday.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                birthdayPicker.show(activity.getSupportFragmentManager(), "birthdayPicker");
+            }
+        });
+
+        ArrayAdapter<Gender> adapter = new ArrayAdapter<>(activity,
+                android.R.layout.simple_spinner_item, Gender.values());
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        gender.setAdapter(adapter);
     }
 
     public Client getClient() {
         client.setPassport(passport.getText().toString());
         client.setFirstName(firstName.getText().toString());
         client.setLastName(lastName.getText().toString());
-        client.setBirthday(FormatUtils.parseDate(birthday.getText().toString()));
-        client.setGender(Gender.valueOf(gender.getText().toString()));
+        client.setGender((Gender) gender.getSelectedItem());
         client.setCountry(Country.fromCode(country.getText().toString()));
         return client;
     }
@@ -47,8 +78,10 @@ class ClientBinding {
         passport.setText(client.getPassport());
         firstName.setText(client.getFirstName());
         lastName.setText(client.getLastName());
-        birthday.setText(FormatUtils.formatDate(client.getBirthday()));
-        gender.setText(client.getGender().toString());
+        LocalDate date = client.getBirthday();
+        birthday.setText(FormatUtils.formatDate(date));
+        birthdayPicker.setPreselectedDate(date.getYear(), date.getMonthValue() - 1, date.getDayOfMonth());
+        gender.setSelection(client.getGender().ordinal());
         country.setText(client.getCountry().getCode());
     }
 }
