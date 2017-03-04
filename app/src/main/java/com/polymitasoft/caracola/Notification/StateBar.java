774653e28562daac1b3d43bb31f89f5e02be27a2
asio <asio@polymitasoft.com>
2017-03-03 18:27:53 -0700

    Arreglar iconos de las notificaciones, Asio commit

--- app/src/main/java/com/polymitasoft/caracola/Notification/StateBar.java
+++ app/src/main/java/com/polymitasoft/caracola/Notification/StateBar.java
@@ -21,14 +21,14 @@ public class StateBar {
     public void notificar(Context context_emisor, Class<CaracolaApplication> context_receptor, String titulo, String text, String info, String ticker, String nota) {
 
         // Patrón de vibración: 1 segundo vibra, 0.5 segundos para, 1 segundo vibra
-        long[] pattern = new long[]{0, 1000, 0};
-        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context_emisor).setSmallIcon(android.R.drawable.sym_def_app_icon).setContentTitle(titulo).setContentText(text).setContentInfo(info).setTicker(ticker);
+        long[] pattern = new long[]{0, 2000, 0};
+        NotificationCompat.Builder mBuilder = (NotificationCompat.Builder) new NotificationCompat.Builder(context_emisor).setSmallIcon(R.drawable.ic_email_black_24dp).setContentTitle(titulo).setContentText(text).setContentInfo(info).setTicker(ticker);
 
         Intent notIntent = new Intent(context_emisor, context_receptor.getClass());
         PendingIntent contIntent = PendingIntent.getActivity(context_emisor, 0, notIntent, 0);
 
         Uri defaultSound = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
-//        mBuilder.setSound(defaultSound);
+        mBuilder.setSound(defaultSound);
         mBuilder.setVibrate(pattern);
         mBuilder.setLights(Color.RED, 1, 0);
 
@@ -37,8 +37,8 @@ public class StateBar {
         Intent intent = new Intent(context_emisor, context_receptor.getClass());
         intent.setAction(Intent.ACTION_VIEW);
 
-//        PendingIntent piDismiss = PendingIntent.getActivity(context_emisor, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        mBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(nota)).addAction(android.R.drawable.sym_def_app_icon, "Confirmar", null).setAutoCancel(true);
+        //        PendingIntent piDismiss = PendingIntent.getActivity(context_emisor, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        mBuilder.setStyle(new NotificationCompat.BigTextStyle().bigText(nota)).addAction(R.drawable.ic_done_black_24dp, "Confirmar", null).setAutoCancel(true);
         mBuilder.setFullScreenIntent(contIntent, true);
 
         NotificationManager mNotificationManager = (NotificationManager) context_emisor.getSystemService(Context.NOTIFICATION_SERVICE);
