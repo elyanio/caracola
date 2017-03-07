9d6d31766ac218d7dfcccc5f1d27fda991f42542
Rainer Martínez Fraga <rmartinezfraga@yandex.com>
2017-03-07 16:45:07 -0500

    Add dp(int)

--- app/src/main/java/com/polymitasoft/caracola/util/Metrics.java
+++ app/src/main/java/com/polymitasoft/caracola/util/Metrics.java
@@ -2,6 +2,8 @@ package com.polymitasoft.caracola.util;
 
 import android.content.Context;
 
+import com.polymitasoft.caracola.CaracolaApplication;
+
 /**
  * @author rainermf
  * @since 22/2/2017
@@ -13,7 +15,15 @@ public class Metrics {
         throw new AssertionError();
     }
 
+    /**
+     * @deprecated Use dp(int)
+     */
+    @Deprecated
     public static int dp(Context context, int value) {
         return (int) (value * context.getResources().getDisplayMetrics().density);
+    }
+
+    public static int dp(int value) {
+        return (int) (value * CaracolaApplication.instance().getResources().getDisplayMetrics().density);
     }
 }
